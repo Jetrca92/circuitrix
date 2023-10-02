11726b135d24e979846e1894227d2a56fde370f8
Jetrca92 <blaz.guzej@gmail.com>
2023-10-02 20:41:49 +0200

    Add custom reg view for now

--- registration/urls.py
+++ registration/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path, reverse_lazy
-from django.contrib.auth.views import LoginView, PasswordResetView, PasswordResetConfirmView, PasswordResetDoneView, LogoutView
-from .views import RegistrationView
+from django.contrib.auth.views import LoginView, PasswordResetView, PasswordResetConfirmView, PasswordResetDoneView, PasswordResetCompleteView, LogoutView
+from . import views
 
 app_name = 'registration'
 
 urlpatterns = [
-    path('register/', RegistrationView.as_view(), name='register'),
+    path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('password_reset/', PasswordResetView.as_view(success_url=reverse_lazy('registration:password_reset_done')), name='password_reset'),
-    path('password_reset_confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password_reset_confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(success_url=reverse_lazy('registration:password_reset_complete')), name='password_reset_confirm'),
     path('password_reset_done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
--- registration/views.py
+++ registration/views.py
@@ -1,12 +1,68 @@
-from .forms import RegistrationForm
+from django.contrib.auth import login
+from django.core.mail import send_mail
+from django.db import IntegrityError, transaction
+from django.dispatch import receiver
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
+from allauth.account.signals import user_signed_up
 from manager.models import User
-from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
 
 
-class RegistrationView(CreateView):
-    form_class = RegistrationForm
-    template_name = 'registration/register.html'
-    success_url = reverse_lazy('manager:index')
+@transaction.atomic
+def register(request):
+    if request.method != "POST":
+        return render(request, "registration/register.html")
+
+    username = request.POST["username"]  
+    email = request.POST["email"]
+
+    # Ensure password matches confirmation
+    password = request.POST["password1"]
+    confirmation = request.POST["password2"]
+    if password != confirmation:
+        return render(request, "registration/register.html", {
+            "message": "Passwords must match."
+        })
+
+    # Attempt to create new user
+    try:
+        user = User.objects.create_user(username, email, password)
+        user.save()
+    except IntegrityError:
+        return render(request, "registration/register.html", {
+            "message": "Username already taken."
+        })
+        
+    # Create users manager model ...
+    #TO DO
+
+    # Logs the user in and sends welcome email
+    login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+    send_mail(
+        'Welcome to Circuitrix!',
+        'Greetings! You have successfully registered on our portal. Good luck!',
+        'settings.EMAIL_HOST_USER',
+        [f"{email}"],
+        fail_silently=True
+    )
+    return HttpResponseRedirect(reverse("manager:index"))
+
+    
+# Sends welcome email if user signs up with google acc    
+@receiver(user_signed_up)
+def send_google_registration_email(request, user, **kwargs):
+    if user.socialaccount_set.filter(provider='google').exists():
+
+        email = user.email
+        send_mail(
+            'Welcome to Circuitrix!',
+            'Greetings! You have successfully registered on our portal. Good luck!',
+            'settings.EMAIL_HOST_USER',
+            [f"{email}"],
+            fail_silently=True
+        )
+    return HttpResponseRedirect(reverse("manager:index"))
 
 
