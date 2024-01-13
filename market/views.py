40521ceb136e61cb06a48333636669d57d7f1918
Jetrca92 <blaz.guzej@gmail.com>
2024-01-13 21:59:38 +0100

    Migrate, add icons to layout, make driver table

--- market/views.py
+++ market/views.py
@@ -2,12 +2,23 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
 from django.shortcuts import render
 
+from manager.models import Driver
+from market.models import DriverListing
 from races.views import ManagerContextMixin
 
-class DriverMarketView(LoginRequiredMixin, View):
+class DriverMarketView(LoginRequiredMixin, ManagerContextMixin, View):
     template_name = "market/market.html"
 
     def get(self, request):
-        return render(request, self.template_name)
+        driver = Driver.objects.all().first()
+        dl = DriverListing(
+            driver=driver,
+            seller=driver.team,
+            price=100,
+        )
+        dl.save()
+        listed_drivers = DriverListing.objects.all()
+        context = {"listed_drivers": listed_drivers}
+        return render(request, self.template_name, context)
     
 
