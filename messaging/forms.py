4df7a0214a22d2a69bfdcca911a225a00b64c606
Jetrca92 <blaz.guzej@gmail.com>
2024-01-16 10:47:52 +0100

    Remove unecessary clean manager

--- messaging/forms.py
+++ messaging/forms.py
@@ -19,12 +19,6 @@ class NewMessageForm(forms.Form):
         label="Content", 
         widget=forms.Textarea(attrs={"class": "form-control", "rows": "16", "maxlength": "1000"}),
     )
-
-    def clean_content(self):
-        content = self.cleaned_data.get("content")
-        if not content:
-            raise forms.ValidationError("You can't send an empty message!")
-        return content
     
     def __init__(self, *args, **kwargs):
         super(NewMessageForm, self).__init__(*args, **kwargs)
