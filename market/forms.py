c20cdc3501d88db1375467aab6c1b9494eb22e8f
Jetrca92 <blaz.guzej@gmail.com>
2024-01-24 20:55:58 +0100

    Edit price matching

--- market/forms.py
+++ market/forms.py
@@ -26,8 +26,8 @@ class DriverBidForm(forms.Form):
         highest_bid = existing_bids.order_by("-amount").first()
         if highest_bid is not None and amount <= highest_bid.amount:
             raise forms.ValidationError("Bid amount must be higher than the highest bid!")
-        if amount <= driver_listing.price:
-            raise forms.ValidationError("Bid amount must be higher than the price!")
+        if amount < driver_listing.price:
+            raise forms.ValidationError("Bid amount must match the default price!")
         if hasattr(self, "bidder") and self.bidder and self.bidder == driver_listing.seller:
             raise forms.ValidationError("You can't bid on your own player!")
         return amount
